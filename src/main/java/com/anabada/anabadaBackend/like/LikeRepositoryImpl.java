4ddbc418678be3e1ac1500775c1f7965ea9ccac8
GiwoongBang <bgw6713@naver.com>
2022-08-19 20:06:50 +0900

    좋아요 기능 구현

--- src/main/java/com/anabada/anabadaBackend/like/LikeRepositoryImpl.java
+++ src/main/java/com/anabada/anabadaBackend/like/LikeRepositoryImpl.java
@@ -6,10 +6,23 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class LikeRepositoryImpl {
+public class LikeRepositoryImpl implements LikeRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
     QLikeEntity like = QLikeEntity.likeEntity;
 
+    @Override
+    public Long findByPostIdAndUserId(Long postId, Long userId) {
+        return queryFactory.select(like.likeId)
+                .from(like)
+                .where(like.post.postId.eq(postId),like.user.userId.eq(userId))
+                .fetchOne();
+    }
 
+    @Override
+    public LikeEntity findOneByUsername(String nickname) {
+        return queryFactory.selectFrom(like)
+                .where(like.user.nickname.eq(nickname))
+                .fetchOne();
+    }
 }
