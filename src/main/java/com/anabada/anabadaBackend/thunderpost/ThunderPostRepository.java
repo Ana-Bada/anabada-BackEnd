c707d6b24d57b663fdbc95b4ae3c30481b72b956
4Sangwon <idcop7@naver.com>
2022-08-22 21:46:14 +0900

    어노테이션 수정

--- src/main/java/com/anabada/anabadaBackend/thunderpost/ThunderPostRepository.java
+++ src/main/java/com/anabada/anabadaBackend/thunderpost/ThunderPostRepository.java
@@ -1,9 +1,7 @@
 package com.anabada.anabadaBackend.thunderpost;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ThunderPostRepository extends JpaRepository<ThunderPostEntity, Long> {
 
 }
--- src/main/java/com/anabada/anabadaBackend/thunderrequest/ThunderRequestRepository.java
+++ src/main/java/com/anabada/anabadaBackend/thunderrequest/ThunderRequestRepository.java
@@ -1,11 +1,9 @@
 package com.anabada.anabadaBackend.thunderrequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ThunderRequestRepository extends JpaRepository<ThunderRequestEntity, Long> {
     ThunderRequestEntity findByThunderPostThunderPostIdAndUserUserId(Long thunderPostId, Long userId);
     List<ThunderRequestEntity> findAllByThunderPostThunderPostId(Long thunderPostId);
