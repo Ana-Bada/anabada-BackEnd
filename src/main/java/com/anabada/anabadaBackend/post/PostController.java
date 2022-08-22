eb3e87ce85a40b9aa02f3ab114f83879b8253d37
4Sangwon <idcop7@naver.com>
2022-08-22 23:34:01 +0900

    게시글 에러 수정

--- src/main/java/com/anabada/anabadaBackend/post/PostController.java
+++ src/main/java/com/anabada/anabadaBackend/post/PostController.java
@@ -4,7 +4,6 @@ import com.anabada.anabadaBackend.post.dto.PostDetailsResponseDto;
 import com.anabada.anabadaBackend.post.dto.PostRequestDto;
 import com.anabada.anabadaBackend.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +17,7 @@ public class PostController {
     @PostMapping("/api/posts")
     public ResponseEntity<?> createPost(@RequestBody PostRequestDto postRequestDto,
                                         @AuthenticationPrincipal UserDetailsImpl userDetails){
-        return new ResponseEntity<>(
-                postService.createPost(postRequestDto, userDetails.getUser()),
-                HttpStatus.valueOf(200)
-        );
+        return postService.createPost(postRequestDto, userDetails.getUser());
     }
 
 
