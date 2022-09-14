308fd2ddab99a0083b33b75c8f9364805ab08edd
4Sangwon <idcop7@naver.com>
2022-09-14 11:50:55 +0900

    사용하지 않는 코드 삭제

--- src/main/java/com/anabada/anabadaBackend/beach/BeachService.java
+++ src/main/java/com/anabada/anabadaBackend/beach/BeachService.java
@@ -6,23 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class BeachService {
     private final BeachRepository beachRepository;
-    
-    public ResponseEntity<?> getBeaches(){
-        List<BeachEntity> beachList = beachRepository.findAll();
-        List<BeachResponseDto> beachResponseDtoList = new ArrayList<>();
-        for(BeachEntity beach : beachList) {
-            BeachResponseDto beachResponseDto = new BeachResponseDto(beach);
-            beachResponseDtoList.add(beachResponseDto);
-        }
-        return new ResponseEntity<>(beachResponseDtoList, HttpStatus.OK);
-    }
 
     public BeachResponseDto getBeachWeather(Long beachId){
         BeachEntity beach = beachRepository.findById(beachId)
