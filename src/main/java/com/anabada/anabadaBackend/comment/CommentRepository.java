164c3eb3dda7edef11b46c4843fd18edf0309ac8
4Sangwon <idcop7@naver.com>
2022-09-21 02:25:11 +0900

    사용하지 않는 코드 삭제

--- src/main/java/com/anabada/anabadaBackend/comment/CommentRepository.java
+++ src/main/java/com/anabada/anabadaBackend/comment/CommentRepository.java
@@ -1,7 +1,5 @@
 package com.anabada.anabadaBackend.comment;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +8,5 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
-    Page<CommentEntity> findAllByPostPostId(Long postId, Pageable pageable);
-    
-    CommentEntity findByPost(Long postId);
     List<CommentEntity> findAllByPostPostId(Long postId);
 }
